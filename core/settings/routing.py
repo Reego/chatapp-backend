d5d622160729b12a87303acd0cacf3f56c1b7a6b
reegodev <reegodev@gmail.com>
2020-01-29 20:28:46 -0600

    Settings changes

--- core/settings/routing.py
+++ core/settings/routing.py
@@ -2,15 +2,14 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 
-import chat.routing
+import apps.chat.routing
 
 application = ProtocolTypeRouter({
     'websocket': AllowedHostsOriginValidator(
         AuthMiddlewareStack(
             URLRouter(
-                chat.routing.websocket_urlpatterns
+                apps.chat.routing.websocket_urlpatterns
             )
-        ),
-        ['']
+        )
     )
 })
